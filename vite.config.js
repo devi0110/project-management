3419ed8850d34abd67df886bc93bfc5ac58194ef
Devi Popuri <139315184+devi0110@users.noreply.github.com>
2024-06-17 22:53:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   export default defineConfig({
-  base: '/your-repo-name/',  // Replace 'your-repo-name' with your actual repository name
+  base: 'project-management',  // Replace 'your-repo-name' with your actual repository name
   build: {
     outDir: 'dist',
   },
